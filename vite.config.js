a0d2139f0a26bc6d56ce04cd1e1c143f586f8ae8
Delemangi <milev.stefan@gmail.com>
2026-02-28 03:43:53 +0100

    Replace @tailwindcss/postcss with @tailwindcss/vite and update configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': resolve(import.meta.dirname, './src'),
