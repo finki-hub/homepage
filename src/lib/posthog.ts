cffe1f2d1f7d498db7b93f4bf54b3938cf79a25d
Delemangi <milev.stefan@gmail.com>
2026-06-27 17:29:02 +0200

    feat: add PostHog analytics

---
+++ src/lib/posthog.ts
@@ -0,0 +1,16 @@
+import { posthog } from 'posthog-js';
+
+export const initPostHog = () => {
+  const key = import.meta.env.VITE_POSTHOG_KEY;
+  if (key === undefined || key === '') {
+    return;
+  }
+
+  posthog.init(key, {
+    // eslint-disable-next-line camelcase -- PostHog SDK option key
+    api_host: import.meta.env.VITE_POSTHOG_HOST ?? 'https://eu.i.posthog.com',
+    autocapture: true,
+    // eslint-disable-next-line camelcase -- PostHog SDK option key
+    person_profiles: 'always',
+  });
+};
